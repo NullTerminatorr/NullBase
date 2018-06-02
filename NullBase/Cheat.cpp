8dda41356815b929e5f8c3781720870f4105be82
Null Terminator <jules.malic@gmail.com>
2018-06-02 11:18:57 +1000

    Updated

--- NullBase/Cheat.cpp
+++ NullBase/Cheat.cpp
@@ -22,7 +22,8 @@ THE USE OR OTHER DEALINGS IN THE SOFTWARE.																						  //
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-/*NEED SOME HELP? FULL TUTORIALS ON HOW TO USE THIS BASE AND CODE CHEATS ON MY YOUTUBE CHANNEL
+/*
+NEED SOME HELP? FULL TUTORIALS ON HOW TO USE THIS BASE AND CODE CHEATS ON MY YOUTUBE CHANNEL:
 Null Terminator
 https://www.youtube.com/channel/UCZF93Qrt6yMAabRnlND4YsQ
 */

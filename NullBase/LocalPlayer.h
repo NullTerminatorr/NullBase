583fcf6cba092f77ec138d30b4d97f67459a6443
Null Terminator <35753575+NullTerminatorr@users.noreply.github.com>
2019-06-28 19:34:12 +1000

    Update LocalPlayer.h

--- NullBase/LocalPlayer.h
+++ NullBase/LocalPlayer.h
@@ -10,16 +10,19 @@ namespace LocalPlayer
 {
 	//Localplayer return functions
 	extern DWORD			getLocalPlayer();												//Get the local player base address
-	extern void				setLocalPlayer();												//Set the local player base address
 	extern int				getLocalFlags();												//Get the local player flags
 	extern int				getLocalHealth();												//Get the local player health
 	extern int				getLocalCrossID();												//Get the local player crosshair ID
 	extern int				getLocalTeam();													//Get the local player team ID
 	extern D3DXVECTOR3		getLocalPos();													//Get the local player pos
-	extern D3DXMATRIX		getViewMatrix();												//Get the view matrix
+	extern D3DXMATRIX		getLocalViewMatrix();											//Get the view matrix
+	extern D3DXVECTOR3		getLocalViewAngles();											//Get the local player view angles
+	extern D3DXVECTOR3		getLocalPunchAngles();											//Get the local player punch angles (recoil)
 
 																							//Void functions 
-	extern void				forceJump();													//Force the local player to jump
+	extern void				forceJump(int waitTime = 35);									//Force the local player to jump
+	extern void				setLocalPlayer();												//Set the local player base address
+	extern void				setLocalViewAngles(D3DXVECTOR3 angles);							//Set the local player view angles
 
 	extern DWORD			LocalBaseaddress;												//Localplayer base address
 }
